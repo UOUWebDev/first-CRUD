cefc930637d92a2da9e885cb4e5db4f845e0821b
Eriniss <jhs2195@gmail.com>
2022-07-04 23:21:06 +0900

    chageMenuStyle

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,31 @@ import ReactDOM from 'react-dom';
 
 function App() {
   return (
-    <div className="header">
-
-      <ul className = "nav">
-        <img className = "logoImg" src="https://th.bing.com/th/id/OIP.TTuXNvbU-dlR4qNpiMjKcwHaGn?w=190&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7" />
-        <li><button onClick = {handlerClick}>공지사항</button></li>
-        <li><button onClick = {handlerClick}>소통게시판</button></li>
-        <li><button onClick = {handlerClick}>건의게시판</button></li>
-        <li><button onClick = {handlerClick}>자유게시판</button></li>
-        <ul className = "login">
-          <li>로그인</li>
-          <li>회원가입</li>
+    <div className="App">
+      <div className="header">
+        <ul className = "nav">
+          <img className = "logoImg" src="https://th.bing.com/th/id/OIP.TTuXNvbU-dlR4qNpiMjKcwHaGn?w=190&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7" />
+          <li><button onClick = {handlerClick}>공지사항</button></li>
+          <li><button onClick = {handlerClick}>소통게시판</button></li>
+          <li><button onClick = {handlerClick}>건의게시판</button></li>
+          <li><button onClick = {handlerClick}>자유게시판</button></li>
+          <ul className = "login">
+            <li>로그인</li>
+            <li>회원가입</li>
+          </ul>
+          <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrOXw0W7pV-_uU38-iNJfaQbfDMJyu6c0--Q&usqp=CAU" className = "menuLogo" />
         </ul>
-        <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrOXw0W7pV-_uU38-iNJfaQbfDMJyu6c0--Q&usqp=CAU" className = "menuLogo" />
-      </ul>
+      </div>
+      <div className="main">
+        <div className="textBox"></div>
+      </div>
     </div>
   )
 };
 
+
 const handlerClick = () => alert('Not performed yet');
+
 
 
 export default App;
