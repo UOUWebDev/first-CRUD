bc12f917492a64bbe3185ff0338fc74c7dd6161f
Eriniss <jhs2195@gmail.com>
2022-07-10 13:49:48 +0900

    refactoring 1.2

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,40 @@
+import styled from "styled-components";
 import React from "react";
+
+const Textbox = styled.div`
+  background-color: white;
+  padding: 20px;
+  margin: 20px;
+  width: 50%;
+  height: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: left;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+`
+
+const Revise = styled.button`
+  display: flex;
+  float: right;
+`
+const Delete = styled.button`
+  display: absolute;
+  float: right;
+  bottom: 20px;
+`
 
 export default function Post() {
   return(
     <div className="main">
-      <div className="textBox">
+      <Textbox>
         <h2>text for test</h2>
         <p>this is prototype of CRUD</p>
-      </div>
+        <Delete>삭제</Delete>
+        <Revise>수정</Revise>
+      </Textbox>
     </div>
   );
 }
--- src/pages/Posting.tsx
+++ src/pages/Posting.tsx
@@ -1,14 +1,55 @@
-import React, { useState , useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const Postbox = styled.div`
+background-color: white;
+    padding: 20px;
+    margin: 20px;
+    width: 50%;
+    height: 50%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: left;
+    border-radius: 15px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+`
+
+const PostTitle = styled.input`
+  display: flex;
+    text-align: center;
+    width: 90%;
+    height: 10%;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 20px;
+`
+
+const PostContent = styled.input`
+  display: flex;
+    text-align: left;
+    vertical-align: top;
+    margin-right: auto;
+    margin-left: auto;
+    width: 90%;
+    height: 70%;
+`
+
+const PostButton = styled.button`
+  display: flex;
+    margin-top: 10px;
+    float: right;
+`
 
 function Post() {
   return (
-    <div className="postBox">
-      <input className = "postTitle"></input>
-      <input className = "postContent"></input>
-      <button className = "postButton">작성</button>
-  </div>
+    <Postbox>
+      <PostTitle></PostTitle>
+      <PostContent></PostContent>
+      <PostButton>작성</PostButton>
+    </Postbox>
   );
 }
 
