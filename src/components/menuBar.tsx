4aa1e119f9bb6ee86f66f8d83caf0b8391e20a35
Eriniss <jhs2195@gmail.com>
2022-08-10 23:27:29 +0900

    refactoringMenuBarStyling

--- src/components/menuBar.tsx
+++ src/components/menuBar.tsx
@@ -7,10 +7,6 @@ const MenuBar = createGlobalStyle`
     padding: 0;
   }
 
-  .header {
-    font-size: 20px;
-  }
-
   .nav {
   font-size: 20px;
   font-weight: bold;
@@ -25,55 +21,45 @@ const MenuBar = createGlobalStyle`
   background-color: white;
   border: 1px solid black;
   padding: 20px;
-  }
 
-  .nav li {
-  display: inline;
-  min-width: 5em;
-  margin: 0 2em;
-  }
+  li {
+    display: inline;
+    min-width: 5em;
+    margin: 0 2em;
 
-  .nav li button:hover {
-  animation-name: drawingLine;
-  animation-duration: 10s;
-  animation-delay: 0.1s;
-  animation-direction: reverse;
-  animation-timing-function: linear;
-  }
+    a {
+      text-decoration: none;
+      color: black;
+      }
 
-  @keyframes drawingLine {
-  from {text-decoration: none;}
-  to {
-    text-decoration: underline;
-    text-decoration-color: lightblue;
     }
-  }
 
   .logoImg {
-  float: left;
-  width: 30px;
-  height: 30px;
-  transform: rotate(90deg);
-  }
+    float: left;
+    width: 30px;
+    height: 30px;
+    transform: rotate(90deg);
+    }
 
-  .nav .menuLogo {
-  font-size: 25px;
-  float: right;
-  width: 40px;
-  height: 30px;
-  }
+  .menuLogo {
+    font-size: 25px;
+    float: right;
+    width: 40px;
+    height: 30px;
+    }
 
-  .nav .login  {
-  display: inline-block;
-  font-size: 15px;
-  text-align: center;
-  margin-left: 10%;
-  padding: 0px;
-  }
+  .login  {
+    display: inline-block;
+    font-size: 15px;
+    text-align: center;
+    margin-left: 10%;
+    padding: 0px;
 
-  .nav .login li {
-  display: inline;
-  margin-right: 0px;
+    li {
+      display: inline;
+      margin-right: 0px;
+      }
+    }
   }
 `;
 
